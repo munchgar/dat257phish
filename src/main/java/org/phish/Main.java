661f8493cbf69cd338222a8c60e836a8b1198a83
HersiZa <44900079+HersiZa@users.noreply.github.com>
2020-09-28 10:36:23 +0200

    Skapade HomeScreen med nxtbtn. Note den dubblerar sidan vet ej vrf

--- src/main/java/org/phish/Main.java
+++ src/main/java/org/phish/Main.java
@@ -5,12 +5,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.phish.database.DBHandler;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Main extends Application {
@@ -29,8 +32,9 @@ public class Main extends Application {
         this.primaryStage.setTitle("Environment calculator");
         showMainView();
         //loadCenter("MainSections.fxml");
-        loadCenter("HomePage.fxml");
+        //loadCenter("HomePage.fxml");
     }
+
 
 
     private void showMainView() throws IOException {
--- src/main/java/org/phish/controllers/HomePageController.java
+++ src/main/java/org/phish/controllers/HomePageController.java
@@ -1,6 +1,9 @@
 package org.phish.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import org.phish.Main;
 
 import java.io.IOException;
@@ -13,8 +16,16 @@ public class HomePageController {
     public void goHome(ActionEvent actionEvent) throws IOException {
         //Main.loadCenter("MainSections.fxml");
         Main.loadCenter("HomePage.fxml");
+        //Finns ett litet bugg där ifall man trycker på "Home" när man är i Homepage då blire dublleter
     }
     public void showMainSec(ActionEvent actionEvent) throws IOException{
         Main.loadCenter("MainSections.fxml");
+    }
+    @FXML
+    private ImageView Img1;
+
+
+    public void init(){
+        Img1.setImage(new Image("@../../../../../../../Downloads/unnamed.png"));
     }
 }
