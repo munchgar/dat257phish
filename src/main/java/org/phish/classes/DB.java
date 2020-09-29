1c240e73858f26fd7c5920b96d1deaa4983673d9
Darclander <lukedarclander@gmail.com>
2020-09-29 14:47:33 +0200

    Implement test DB interface
    An idea for a DB interface which can use a selector method.

---
+++ src/main/java/org/phish/classes/DB.java
@@ -0,0 +1,41 @@
+package org.phish.classes;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public interface DB {
+
+    static final String url = "jdbc:sqlite:src/main/java/org/phish/database/userDB.db";
+
+    static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    static ResultSet select(String retrieve, String table) {
+
+        String SQLquery = "SELECT fName, lName FROM userTable"; // Do we need * or only fName and lName?
+        try (Connection conn = connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(SQLquery)){
+            while(rs.next()){
+                try {
+                    return rs;
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+
+        return null;
+    }
+
+}
