6682303b9b56b59868f04aa55b70a330178100e2
Pontare25 <pontus.nellgard@gmail.com>
2020-09-29 18:10:32 +0200

    The user can now add a vehicle to their DB collection. A check needs to be added to make sure the efficiencyField is a double.

--- src/main/java/org/phish/controllers/VehiclesPageController.java
+++ src/main/java/org/phish/controllers/VehiclesPageController.java
@@ -1,14 +1,23 @@
 package org.phish.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import org.phish.Main;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class VehiclesPageController {
+public class VehiclesPageController implements Initializable {
+
 
     public void openAddVehicle(ActionEvent actionEvent) throws IOException {
         //todo open add new vehicle window
         Main.showModalWindow("AddVehicleWindow.fxml", "Add Vehcile");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
