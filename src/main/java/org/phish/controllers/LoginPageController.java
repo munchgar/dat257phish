d53d33cc873407fe6ce3c466a228f92eccd103ea
Darclander <lukedarclander@gmail.com>
2020-10-13 14:40:07 +0200

    Login fixed

--- src/main/java/org/phish/controllers/LoginPageController.java
+++ src/main/java/org/phish/controllers/LoginPageController.java
@@ -2,17 +2,27 @@ package org.phish.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.phish.classes.DB;
 import org.phish.database.DBHandler;
+import org.phish.Main;
 
 
+import java.io.IOException;
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 
-public class LoginPageController {
+
+public class LoginPageController implements Initializable {
+
 
     @FXML
     public TextField idUsername;
@@ -20,30 +30,58 @@ public class LoginPageController {
     public TextField idPassword;
     @FXML
     public Button signupBtn;
+    @FXML
+    public Text fieldsFilledCheckText;
+    @FXML
+    public Text wrongInputCheckText;
 
-    public void login() throws SQLException {
+    public int login() throws SQLException, IOException {
         DBHandler dbHandler = new DBHandler();
+
+        if(!idUsername.getText().isBlank() && !idPassword.getText().isBlank()){
+            //System.out.println(fNameField.getText() + " " + lNameField.getText());
+            if(fieldsFilledCheckText.isVisible()){
+                fieldsFilledCheckText.setVisible(false);
+            }
+        }
+        else {
+            // System.out.println("All textfields must be filled");
+            fieldsFilledCheckText.setVisible(true);
+            fieldsFilledCheckText.setFill(Color.RED);
+        }
 
         if(dbHandler.connect()) { // Attempt to connect to database.
             ResultSet rs = dbHandler.execQuery("SELECT * FROM userTable"); // Execute query
             while (rs.next()) {
-                System.out.println(rs.getString("username"));
                 if (rs.getString("username").equals(idUsername.getText()) && (rs.getString("password").equals(idPassword.getText()))) {
                     System.out.println("USER LOGGED IN"); // Implement login for user ID PRIMARY KEY IN DB
+                    System.out.println(rs.getString("username"));
+                    if (dbHandler.connect()) {
+                        ResultSet rs2 = dbHandler.execQuery("SELECT userId FROM userTable WHERE username = '" + rs.getString("username") + "'");
+                        // return rs2.getInt("userId"); // No reason for a return statement here?
+                        Main.setCurrentUserId(rs.getInt("userId"));
+                        Main.showMainView();
+                    }
+                } else {
+                    if(!fieldsFilledCheckText.isVisible()) {
+                        wrongInputCheckText.setVisible(true);
+                        wrongInputCheckText.setFill(Color.RED);
+                    }
                 }
             }
         }
+        return -1; // Login failed, also no reason for a return?
     }
 
-    public void closeWindow(ActionEvent actionEvent) {
-   /*     FXMLLoader loader = new FXMLLoader(getClass().getResource("AddNewUser.fxml"));
-        OwnController controller = loader.getController();
-        controller.setStage(this.stage);
-    */
+    public void closeWindow(ActionEvent actionEvent) throws IOException {
+        Main.showMainView();
+    }
 
-        //Returns the stage associated with the button. In this case, the cancel button
-        Stage stage = (Stage) signupBtn.getScene().getWindow();
-        stage.close();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        fieldsFilledCheckText.setVisible(false);
+        wrongInputCheckText.setVisible(false);
     }
 
 }
