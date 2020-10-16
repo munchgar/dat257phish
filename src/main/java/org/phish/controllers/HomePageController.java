5bb8092e7658a0c962720069777a0622cb820f55
hersiza <Zakariahersi@outlook.com>
2020-10-16 19:04:42 +0200

    Signing out works, minor (yet to implement) bug where u can sign out without signing in. Priority low.

--- src/main/java/org/phish/controllers/HomePageController.java
+++ src/main/java/org/phish/controllers/HomePageController.java
@@ -5,12 +5,22 @@ import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import org.phish.Main;
+import org.phish.database.DBHandler;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class HomePageController {
     public void closeApplication(ActionEvent actionEvent) {
         System.exit(0);
+    }
+
+    public void signOut (ActionEvent actionEvent) throws IOException {
+        System.out.println("User signed out");
+
+        Main.showMainView();
+
     }
 
     public void goHome(ActionEvent actionEvent) throws IOException {
