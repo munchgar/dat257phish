999bd5c804a9232e7dd348c6ac37916a86d95c90
Darclander <lukedarclander@gmail.com>
2020-10-19 15:35:48 +0200

    Revert "Merge branch 'master' of https://github.com/munchgar/dat257phish"
    This reverts commit b153b9db01f3c4b849d3ec3234536692348c3d20, reversing
    changes made to bd19e817e892d35446ca685b2c579536c2679e4b.

--- src/main/java/org/phish/controllers/TransportActivitiesController.java
+++ src/main/java/org/phish/controllers/TransportActivitiesController.java
@@ -8,6 +8,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import org.phish.Main;
 import org.phish.classes.TransportActivity;
 import org.phish.classes.User;
@@ -53,6 +55,9 @@ public class TransportActivitiesController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        errorText.setText("You must add a vehicle\nbefore you can add an activity");
+        errorText.setFill(Color.RED);
+        errorText.setVisible(false);
         setUpTable();
         try {
             loadData();
@@ -104,8 +109,14 @@ public class TransportActivitiesController implements Initializable {
                         }
                     }
                     //TransportActivity(int activityId, int userId, int distance, String date, String activityName, Vehicle vehicle)
-                    transportActivities.add(new TransportActivity(rs.getInt("transportActivityId"), rs.getInt("FKuserId"), rs.getInt("distanceKm"),
-                            rs.getString("date"), rs.getString("activityName"), vehicles.get(index)));
+                    if(index == -1) {
+                        errorText.setVisible(true);
+                        System.out.println("ERROR NO CARS");
+                    } else {
+                        errorText.setVisible(false);
+                        transportActivities.add(new TransportActivity(rs.getInt("transportActivityId"), rs.getInt("FKuserId"), rs.getInt("distanceKm"),
+                                rs.getString("date"), rs.getString("activityName"), vehicles.get(index)));
+                    }
                 }catch (Exception ex){
                     ex.printStackTrace();
                 }
