bab0b3598023d2f21604614f041367d6803e236a
Darclander <lukedarclander@gmail.com>
2020-10-19 15:35:46 +0200

    Revert "Update TransportActivitiesController.java"
    This reverts commit 4aa8afa65bb7d4487a57dcdf3258446e7d49c27b.

--- src/main/java/org/phish/controllers/TransportActivitiesController.java
+++ src/main/java/org/phish/controllers/TransportActivitiesController.java
@@ -8,7 +8,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.text.Text;
 import org.phish.Main;
 import org.phish.classes.TransportActivity;
 import org.phish.classes.User;
